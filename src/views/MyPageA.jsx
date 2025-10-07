72a8ff2e97b3e7e2cdeaa4983f3032fdeda3ec83
bankneedtosleep <bank03124@gmail.com>
2025-10-07 15:56:17 +0700

    1

---
+++ src/views/MyPageA.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Hello from '../assets/imgA/Hello.png'
+import Profile from '../assets/imgA/Profile.png'
+export default function MyPageA() {
+  return (
+    <div className='w-full h-flex flex flex-col items-center bg-black'>
+      
+    </div>
+  )
+}
---
+++ src/views/NotFound.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {GiCancel} from 'react-icons/gi'
+export default function NotFound() {
+  return (
+    <>
+        <div>
+          <h1 className='text-3xl text-center font-bold text-red-600'>
+            <>GiCancel className='mx-auto text-4x1</>
+            ไม่พบหน้าที่ต้องการ
+            <br/>กรุณาลองใหม่
+            <br />หรือตรวจสอบ URL
+          </h1>
+        </div>
+    </>
+  )
+}
